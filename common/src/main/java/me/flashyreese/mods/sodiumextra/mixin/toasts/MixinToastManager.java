bc7e2c858e5fb71f10d0478cad43102dbad29041
mitw153 <mitw153@tuta.io>
2024-09-22 05:32:53 +0100

    Fix toasts sounds playing when disabled

--- common/src/main/java/me/flashyreese/mods/sodiumextra/mixin/toasts/MixinToastManager.java
+++ common/src/main/java/me/flashyreese/mods/sodiumextra/mixin/toasts/MixinToastManager.java
@@ -1,7 +1,7 @@
 package me.flashyreese.mods.sodiumextra.mixin.toasts;
 
 import me.flashyreese.mods.sodiumextra.client.SodiumExtraClientMod;
-import net.minecraft.client.gui.components.toasts.ToastComponent;
+import net.minecraft.client.gui.components.toasts.*;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,9 +10,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ToastComponent.class)
 public class MixinToastManager {
     @Inject(method = "addToast", at = @At("HEAD"), cancellable = true)
-    public void goodByeToasts(CallbackInfo ci) {
-        if (!SodiumExtraClientMod.options().extraSettings.toasts) {
-            ci.cancel();
+    public void goodByeToasts(Toast toast, CallbackInfo ci) {
+        if ((!SodiumExtraClientMod.options().extraSettings.toasts) ||
+                (!SodiumExtraClientMod.options().extraSettings.tutorialToast && toast instanceof TutorialToast) ||
+                (!SodiumExtraClientMod.options().extraSettings.systemToast && toast instanceof SystemToast) ||
+                (!SodiumExtraClientMod.options().extraSettings.recipeToast && toast instanceof RecipeToast) ||
+                (!SodiumExtraClientMod.options().extraSettings.advancementToast && toast instanceof AdvancementToast)) {
+           ci.cancel();
         }
     }
 }
